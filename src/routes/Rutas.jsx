6a77a5aee8911623d4e5e12359c8a90019c14488
CesarPetit21480 <cesarpetit@gmail.com>
2023-01-28 09:46:56 -0300

    esquema

---
+++ src/routes/Rutas.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const Rutas = () => {
+  return (
+    <div>rutas</div>
+  )
+}
+
+export default Rutas
