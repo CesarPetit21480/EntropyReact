a3a33be458909ea1e64b321c95f62ddb8ef4409a
CesarPetit21480 <cesarpetit@gmail.com>
2023-02-05 10:04:14 -0300

    login

--- src/view/Login/Login.jsx
+++ src/view/Login/Login.jsx
@@ -1,88 +1,61 @@
 import React from "react";
 import "./Login.scss";
-import "./animacion.scss";
-
 const imgRoutes = require.context("../../assets/logo", true);
 const Login = () => {
   const lg = "./logo.png";
+  // const princi = "./principal.jpg";
   return (
-    <section className="vh-100 principal">
-      <div className="container-fluid ">
-        <div className="row">
-        <div className="col-sm-4 text-black"></div>
-          <div className="col-sm-4 text-black recuadro">
-            
-            
-            <div className="px-5 ms-xl-4">
-              <span>
-                <div className="d-flex justify-content-center align-items-center">
-                <img
-                  className="logoTam"
-                  src={imgRoutes(`${lg}`)}
-                  alt="remera"
-                />
-                </div>   
-                <div className="d-flex justify-content-center align-items-center">
-                  {/* <h1 className="t-stroke t-shadow-halftone">ENTROPY</h1> */}
-                  <h1>ENTROPY</h1>
-                </div>
-              </span>
+    <div className="principal container-fluid">
+      <div className="d-flex justify-content-center flex-column log">
+        <img className="logoTam" src={imgRoutes(`${lg}`)} alt="logo" />
+        <div className="positionTitulo">
+          <h2>ENTROPY</h2>
+        </div>
+
+        <div>
+          <form>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                className="form-control inpt"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <small id="emailHelp" className="form-text text-muted">
+                We'll never share your email with anyone else.
+              </small>
             </div>
-            <div className="d-flex justify-content-center align-items-center px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
-              <form style={{ width: "23rem" }}>
-                <h3 className="fw-normal mb-3 pb-3">Ingreso</h3>
-
-                <div className="form-outline mb-4">
-                  <input
-                    type="email"
-                    id="form2Example18"
-                    className="form-control form-control-lg"
-                  />
-                  <label className="form-label" htmlFor="form2Example18">
-                    Email address
-                  </label>
-                </div>
-
-                <div className="form-outline mb-4">
-                  <input
-                    type="password"
-                    id="form2Example28"
-                    className="form-control form-control-lg"
-                  />
-                  <label className="form-label" htmlFor="form2Example28">
-                    Password
-                  </label>
-                </div>
-
-                <div className="pt-1 mb-4">
-                  <button
-                    className="btn btn-info btn-lg btn-block"
-                    type="button"
-                  >
-                    Login
-                  </button>
-                </div>
-
-                <p className="small mb-5 pb-lg-2">
-                  <a className="text-muted" href="#!">
-                    Forgot password?
-                  </a>
-                </p>
-                <p>
-                  Don't have an account?{" "}
-                  <a href="#!" className="link-info">
-                    Register here
-                  </a>
-                </p>
-              </form>
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                className="form-control inpt"
+                id="exampleInputPassword1"
+                placeholder="Password"
+              />
             </div>
-          </div>
-        
-        
-          <div className="col-sm-4 text-black"></div>
+            <div className="form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+              />
+              <label className="form-check-label" htmlFor="exampleCheck1">
+                Check me out
+              </label>
+            </div>
+
+            <div class=" row d-flex justify-content-center align-content-center inpt ">
+              <button type="submit" className="btn btn-dark ">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
